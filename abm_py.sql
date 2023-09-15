738e9c05ac2c5ea41dd8dce07a3241a2c3d1ca8e
juliangaldamez <juliangaldamez0@gmail.com>
2023-09-14 23:51:58 -0300

    archivo mysql que contiene las tablas

--- abm_py.sql
+++ abm_py.sql
@@ -28,10 +28,13 @@ CREATE TABLE `proveedor` (
   `telefono_proveedor` bigint DEFAULT NULL,
   `domicilio_proveedor` varchar(45) NOT NULL,
   `correo_proveedor` varchar(50) NOT NULL,
+  `provincia_proveedor` int NOT NULL,
   PRIMARY KEY (`id_proveedor`),
   UNIQUE KEY `correo_proveedor_UNIQUE` (`correo_proveedor`),
-  UNIQUE KEY `telefono_proveedor_UNIQUE` (`telefono_proveedor`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
+  UNIQUE KEY `telefono_proveedor_UNIQUE` (`telefono_proveedor`),
+  KEY `id_provincia_idx` (`provincia_proveedor`),
+  CONSTRAINT `id_provincia` FOREIGN KEY (`provincia_proveedor`) REFERENCES `provincias` (`idprovincia`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,8 +43,32 @@ CREATE TABLE `proveedor` (
 
 LOCK TABLES `proveedor` WRITE;
 /*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
-INSERT INTO `proveedor` VALUES (1,'juan gomez',2604551200,'rivadavia 1500','juangomez55@gmail.com'),(2,'maria rodriguez',2604551001,'olascoaga 123','mariarodriguez1@gmail.com'),(3,'pedro lopez',2604551002,'santiago del estero 422','pedrolopez2@gmail.com'),(4,'ana martinez',2604551003,'san martin 921','anamartinez3@gmail.com'),(5,'luis perez',2604551004,'paunero 555','luisperez4@gmail.com'),(6,'laura garcia',2604551005,'rawson 847','lauragarcia5@gmail.com'),(7,'diego fernandez',2604551006,'san juan 200','diegofernandez6@gmail.com'),(8,'sofia diaz',2604551007,'matienzo 1279','sofiadiaz7@gmail.com'),(9,'carlos sanchez',2604551008,'anglat 881','carlossanchez8@gmail.com'),(10,'elena ramirez',2604551009,'uriburu 430','elenaramirez9@gmail.com');
+INSERT INTO `proveedor` VALUES (1,'juan gomez',2604551200,'rivadavia 1500','juangomez55@gmail.com',1),(2,'maria rodriguez',2604551001,'olascoaga 123','mariarodriguez1@gmail.com',2),(3,'pedro lopez',2604551002,'santiago del estero 422','pedrolopez2@gmail.com',3),(4,'ana martinez',2604551003,'san martin 921','anamartinez3@gmail.com',4),(5,'luis perez',2604551004,'paunero 555','luisperez4@gmail.com',5),(6,'laura garcia',2604551005,'rawson 847','lauragarcia5@gmail.com',6),(7,'diego fernandez',2604551006,'san juan 200','diegofernandez6@gmail.com',7),(8,'sofia diaz',2604551007,'matienzo 1279','sofiadiaz7@gmail.com',8),(9,'carlos sanchez',2604551008,'anglat 881','carlossanchez8@gmail.com',9),(10,'elena lopez',2604551009,'uriburu  430','elenaramirez9@gmail.com',10),(12,'Ramiro Cora',2625305127,'Castelli   1449','corraramiro02@gmail.com',11),(14,'julian galdamez',2604600688,'anglat        881','juliangaldamez0@gmail.com',13),(17,'david veisaga',2604511225,'paunero 123','davidveisaga@gmail.com',18);
 /*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `provincias`
+--
+
+DROP TABLE IF EXISTS `provincias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `provincias` (
+  `idprovincia` int NOT NULL AUTO_INCREMENT,
+  `nombre_provincia` varchar(45) NOT NULL,
+  PRIMARY KEY (`idprovincia`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `provincias`
+--
+
+LOCK TABLES `provincias` WRITE;
+/*!40000 ALTER TABLE `provincias` DISABLE KEYS */;
+INSERT INTO `provincias` VALUES (1,'Buenos Aires'),(2,'Ciudad Autónoma de Buenos Aires'),(3,'Catamarca'),(4,'Chaco'),(5,'Chubut'),(6,'Córdoba'),(7,'Corrientes'),(8,'Entre Ríos'),(9,'Formosa'),(10,'Jujuy'),(11,'La Pampa'),(12,'La Rioja'),(13,'Mendoza'),(14,'Misiones'),(15,'Neuquén'),(16,'Río Negro'),(17,'Salta'),(18,'San Juan'),(19,'San Luis'),(20,'Santa Cruz'),(21,'Santa Fe'),(22,'Santiago del Estero'),(23,'Tierra del Fuego'),(24,'Tucumán');
+/*!40000 ALTER TABLE `provincias` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -53,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-06 18:13:48
+-- Dump completed on 2023-09-14 23:49:28
